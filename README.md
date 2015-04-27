a7a157c3b4114886c53cfbf9a28f86aa8269ee9b
darrenjaworski@gmail.com <darrenjaworski@gmail.com>
2015-04-26 21:32:14 -0500

    Added readme and bill api call.

---
+++ README.md
@@ -0,0 +1,13 @@
+Install sunlight via pip
+
+```bash
+$ pip install sunlight
+```
+
+Create file .sunlight.key in user root.
+
+```bash
+$ nano ~/.sunlight.key
+```
+
+.sunlight.key should contain your API key.
--- openstates_ok.py
+++ openstates_ok.py
@@ -1,15 +1,9 @@
-import urllib2, json
+from sunlight import openstates
 
-API_KEY = '70f72edb0e034b10bc328c468e591b54'
+oklahoma_bills = openstates.bills(
+    state='ok',
+    chamber='lower'
+)
 
-def get_bills(state, chamber):
-
-    response = urllib2.urlopen('http://openstates.org/api/v1/bills/?state=%s&session=2015-2016&chamber=%s&type=bill&apikey=%s' % (state, chamber, API_KEY))
-    json_object = json.load(response)
-
-    for result in json_object:
-        print result['bill_id']
-
-    return
-
-get_bills('ok', 'lower')
+for bill in oklahoma_bills:
+    print bill['title']

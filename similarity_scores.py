b6a57f5242f4e209191e11e0106c4bb9c4fafc51
darrenjaworski@gmail.com <darrenjaworski@gmail.com>
2015-04-27 15:41:55 -0500

    pseudocode for the similarity scores.

---
+++ similarity_scores.py
@@ -0,0 +1,12 @@
+# This script will read the data from openstates_ok.py
+# and create a similarity score for each legislator to
+# one another.
+
+# for legislator in legislators
+    # compare legislator 1 to every other by bill votes
+    # for legislator in legislators
+
+        # for bill in bills
+            # generate scores based on similarity
+
+        # write score for legislator 1 to every other

bb3d8d6de0bdf131a46eee978903c313b2942a4d
darrenjaworski@gmail.com <darrenjaworski@gmail.com>
2015-04-30 15:27:23 -0500

    Added comments, got rid of some test logs.

--- similarity_scores.py
+++ similarity_scores.py
@@ -5,57 +5,75 @@
 import csv
 from sunlight import openstates
 
+# get list of legislators
 ok_legislators = openstates.legislators(
     state='ok',
     active='true'
 )
 
-# ok_legislators_array = ['legi_id']
+# redundant, but I'm using a DictWriter key
+# and one array to use in loops
 ok_legislators_csv_key = ['leg_id']
 ok_legislators_array = []
 for legislator in ok_legislators:
     ok_legislators_csv_key.append(legislator['leg_id'])
     ok_legislators_array.append(legislator['leg_id'])
 
+# new csv file that we're going to write our matrix of values to
 with open('scores.csv', 'w') as w:
     writer = csv.DictWriter(w, fieldnames=ok_legislators_csv_key, extrasaction='ignore')
     writer.writeheader()
 
+    # first loop of legislators
     for legislatorA in ok_legislators_array:
         print "Going through " + legislatorA
 
+        # our dictionary for each row
+        # we add the first column label
         leg_scores = {}
         leg_scores['leg_id'] = legislatorA
 
+        # second loop of legislators so we can
+        # compare one legislator to another
         for legislatorB in ok_legislators_array:
-            #print "Comparing " + legislatorA + " to " + legislatorB
 
+            # open the votes csv file
             with open('votes.csv') as f:
                 reader = csv.DictReader(f)
 
+                # these variables are to calculate our similarity scores
                 voteCount = 0
                 voteSame = 0
                 notComparable = 0
 
+                # since we opened the votes file
+                # each row in the file represents a bill
                 for bill in reader:
 
+                    # comparing the votes to calculate similarity score
                     if not bill[legislatorA] or not bill[legislatorB]:
+
                         notComparable += 1
-                        #print "not comparable"
+
                     elif bill[legislatorA] == bill[legislatorB]:
+
                         voteCount += 1
                         voteSame += 1
-                        #print "same vote"
-                    else:
-                        voteCount += 1
-                        #print "different vote"
 
+                    else:
+
+                        voteCount += 1
+
+                # I noticed some of the legislators aren't voting on all the bills together,
+                # probably because there is a mix of upper and lower house members.
+                # So we catch any exception with no voteCount, meaning a division by zero.
                 try:
                     score = float(voteSame) / voteCount
                     leg_scores[legislatorB] = score
 
                 except ZeroDivisionError:
-                    #print "No votes were comparable."
                     leg_scores[legislatorB] = "x"
 
+        # write a row for one legislator into the csv
+        # with similarity scores amongst all other legislators
         writer.writerow(leg_scores)
